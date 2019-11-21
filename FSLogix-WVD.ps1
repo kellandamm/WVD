3b41d0eafcbd99eceed5f87d56c3ef154b6aafdf
kellandamm <kellandamm@users.noreply.github.com>
2019-11-21 10:12:05 -0600

    Updated Temppath variables

--- FSLogix-WVD.ps1
+++ FSLogix-WVD.ps1
@@ -9,10 +9,10 @@ Write-Host "Creating Temporary folder for Install"
         
 #Installing Fslogix
 Write-Host "Downloading and Installing FSLogix"
-    Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
+    Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile '$temppath\fslogix.zip'
           Start-Sleep -Seconds 20
-                Expand-Archive -Path C:\temp\fslogix.zip -DestinationPath C:\temp\fslogix\  -Force
-                    Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
+                Expand-Archive -Path "$temppath\fslogix.zip" -DestinationPath "$temppath\fslogix\"  -Force
+                    Invoke-Expression -Command "$temppath\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
 
 #Configuring Registry      
 Write-host "Configuring Registry Entries for FSLogix"
