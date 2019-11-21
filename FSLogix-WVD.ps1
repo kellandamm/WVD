849e400ea4a71efaad08aba78124061afa59f080
kellandamm <kellandamm@users.noreply.github.com>
2019-11-21 10:02:03 -0600

    Add files via upload
    Installs FSLogix with Profile and Office registry entries. Use link for advance Office modification 
    https://docs.microsoft.com/en-us/fslogix/configure-office-container-tutorial

---
+++ FSLogix-WVD.ps1
@@ -0,0 +1,24 @@
+#Set Variables for VHDLocations
+$vhdprofile = "\\fslogix\profiles"
+$vhdoffice = "\\fslogix\office"
+$temppath = "C:\temp"
+
+#Creating a Temp Folder for install
+Write-Host "Creating Temporary folder for Install"
+    New-Item -Path "$temppath" -ItemType Directory -Force | Out-Null
+        
+#Installing Fslogix
+Write-Host "Downloading and Installing FSLogix"
+    Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
+          Start-Sleep -Seconds 20
+                Expand-Archive -Path C:\temp\fslogix.zip -DestinationPath C:\temp\fslogix\  -Force
+                    Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
+
+#Configuring Registry      
+Write-host "Configuring Registry Entries for FSLogix"
+    New-Item hklm:\software\FSLogix\Profiles -Force
+    New-ItemProperty -Path hklm:\software\FSLogix\Profiles -Name Enabled -Value 1 -PropertyType DWORD -Force
+    New-ItemProperty -Path hklm:\software\FSLogix\Profiles -Name VHDLocations -Value "$vhdprofile" -PropertyType String  -Force
+    New-Item hklm:\software\policies\FSLogix\ODFC -Force
+    New-ItemProperty -Path hklm:\software\policies\FSLogix\ODFC -Name Enabled -Value 1 -PropertyType DWORD -Force
+    New-ItemProperty -Path hklm:\software\policies\FSLogix\ODFC -Name VHDLocations -Value "$vhdoffice" -PropertyType String  -Force   
