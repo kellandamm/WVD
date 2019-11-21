51263b0cdf3041a69f2c4c06e704e2e9ad0b8df9
kellandamm <kellandamm@users.noreply.github.com>
2019-11-21 10:26:15 -0600

    Updated with variable changes

--- FSLogix-WVD.ps1
+++ FSLogix-WVD.ps1
@@ -1,19 +1,18 @@
 #Set Variables for VHDLocations
 $vhdprofile = "\\fslogix\profiles"
 $vhdoffice = "\\fslogix\office"
-$temppath = "C:\temp"
+$temppath = "temp"
 
 #Creating a Temp Folder for install
 Write-Host "Creating Temporary folder for Install"
-    New-Item -Path "$temppath" -ItemType Directory -Force | Out-Null
+    New-Item -Path "C:\$temppath\" -ItemType Directory -Force | Out-Null
         
 #Installing Fslogix
 Write-Host "Downloading and Installing FSLogix"
-    Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile '$temppath\fslogix.zip'
+    Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile "C:\$temppath\fslogix.zip"
           Start-Sleep -Seconds 20
-                Expand-Archive -Path "$temppath\fslogix.zip" -DestinationPath "$temppath\fslogix\"  -Force
-                    Invoke-Expression -Command "$temppath\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
-
+                Expand-Archive -Path "C:\$temppath\fslogix.zip" -DestinationPath "C:\$temppath\fslogix\"  -Force
+                    Invoke-Expression -Command "C:\$temppath\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart"
 #Configuring Registry      
 Write-host "Configuring Registry Entries for FSLogix"
     New-Item hklm:\software\FSLogix\Profiles -Force
@@ -21,4 +20,4 @@ Write-host "Configuring Registry Entries for FSLogix"
     New-ItemProperty -Path hklm:\software\FSLogix\Profiles -Name VHDLocations -Value "$vhdprofile" -PropertyType String  -Force
     New-Item hklm:\software\policies\FSLogix\ODFC -Force
     New-ItemProperty -Path hklm:\software\policies\FSLogix\ODFC -Name Enabled -Value 1 -PropertyType DWORD -Force
-    New-ItemProperty -Path hklm:\software\policies\FSLogix\ODFC -Name VHDLocations -Value "$vhdoffice" -PropertyType String  -Force   
+    New-ItemProperty -Path hklm:\software\policies\FSLogix\ODFC -Name VHDLocations -Value "$vhdoffice" -PropertyType String -Force
